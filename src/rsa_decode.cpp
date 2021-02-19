b2e2186b0ec62eae39e4b44c34a332d6d9073c0a
Robert Southee <rksouthee@gmail.com>
2021-02-19 11:46:19 +0000

    Remove base64 encoding from decoder
    base64 encoding has now been removed from key generation, encoding and
    decoding now. It will be possible to combine the encode/decode functions
    as a single function, where customization can be achieved through
    iterator adaptors. In the future we will want to avoid doing encoding
    and decoding on individual characters and instead use a large block
    size.
    
    Fixes #1

--- src/rsa_decode.cpp
+++ src/rsa_decode.cpp
@@ -2,93 +2,60 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <tuple>
 #include <vector>
 #include <boost/multiprecision/cpp_int.hpp>
-#include "base64.h"
-#include "rsa.h"
-#include "rsa_code.h"
 
 using namespace boost::multiprecision;
 typedef number<cpp_int_backend<>, et_off> int_type;
 
+template <typename I, typename O>
+O decode(I f_i, I l_i, const int_type& key, const int_type& modulus, O f_o)
+{
+    while (f_i != l_i) {
+        int_type value = *f_i++;
+        value = powm(value, key, modulus);
+        *f_o++ = value.template convert_to<char>();
+    }
+    return f_o;
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 4) {
-        std::cerr << "Usage: rsa-decode private_key input output\n";
+        std::cerr << "Usage: rsa-decode <private_key> <input> <output>\n";
         return EXIT_FAILURE;
     }
 
     const char* private_key_filename = argv[1];
     std::ifstream private_key_file(private_key_filename);
     if (!private_key_file) {
-        std::cerr << "rsa-decode: error: ";
-        std::cerr << private_key_filename << ": ";
-        std::cerr << strerror(errno) << '\n';
-        return EXIT_FAILURE;
-    }
-
-    std::vector<char> private_key_base64(
-        (std::istream_iterator<char>(private_key_file)),
-         std::istream_iterator<char>());
-    size_t expected_size = rks::base64_size_encoded(
-        RSA_PRIVATE_KEY_SIZE + RSA_MODULUS_KEY_SIZE);
-    if (private_key_base64.size() != expected_size) {
-        std::cerr << private_key_filename << ": error: ";
-        std::cerr << "bad private key size\n";
-        return EXIT_FAILURE;
-    }
-
-    std::vector<unsigned char> data;
-    auto t = rks::base64_decode(private_key_base64.begin(),
-                                private_key_base64.size(),
-                                std::back_inserter(data));
-    if (std::get<1>(t) != 0) {
-        std::cerr << private_key_filename << ": error: ";
-        std::cerr << "bad base64 format\n";
+        std::cerr << "rsa-decode: error: " << private_key_filename << ": " << strerror(errno) << '\n';
         return EXIT_FAILURE;
     }
 
     int_type prv, n;
-    import_bits(prv, data.begin(), data.begin() + RSA_PRIVATE_KEY_SIZE);
-    import_bits(n, data.begin() + RSA_PRIVATE_KEY_SIZE, data.end());
+    private_key_file >> prv >> n;
 
     const char* input_filename = argv[2];
     std::ifstream input_file(input_filename);
     if (!input_file) {
-        std::cerr << "rsa-decode: error: ";
-        std::cerr << input_filename << ": ";
-        std::cerr << strerror(errno) << '\n';
+        std::cerr << "rsa-decode: error: " << input_filename << ": " << strerror(errno) << '\n';
         return EXIT_FAILURE;
     }
 
-    std::vector<char> input_base64(
-        (std::istream_iterator<char>(input_file)),
-         std::istream_iterator<char>());
-    data.clear();
-    t = rks::base64_decode(input_base64.begin(),
-                           input_base64.size(),
-                           std::back_inserter(data));
-    if (std::get<1>(t) != 0) {
-        std::cerr << input_filename << ": error: ";
-        std::cerr << "bad base64 format\n";
-        return EXIT_FAILURE;
-    }
+    std::vector<int_type> input;
+    std::copy(std::istream_iterator<int_type>(input_file), std::istream_iterator<int_type>(), std::back_inserter(input));
 
-    std::vector<unsigned char> decoded;
-    encode(data.begin(), data.size(), RSA_DECODE_BLOCK_SIZE,
-           std::back_inserter(decoded), prv, n);
+    std::string output;
+    decode(input.begin(), input.end(), prv, n, std::back_inserter(output));
 
     const char* output_filename = argv[3];
     std::ofstream output_file(output_filename);
     if (!output_file) {
-        std::cerr << "rsa-decode: error: ";
-        std::cerr << output_filename << ": ";
-        std::cerr << strerror(errno) << '\n';
+        std::cerr << "rsa-decode: error: " << output_filename << ": " << strerror(errno) << '\n';
         return EXIT_FAILURE;
     }
 
-    std::copy(decoded.begin(), decoded.end(),
-              std::ostream_iterator<char>(output_file));
+    output_file << output;
 }
 
