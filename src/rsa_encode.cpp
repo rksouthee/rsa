866d78fae902a0fa4cf05d5508c85cb770edd19e
Robert Southee <rksouthee@gmail.com>
2021-02-19 11:28:34 +0000

    Remove base64 encoding from the encoder
    The RSA key generator outputs the key as decimal numbers rather than
    base64 encoding. Similarly instead of outputting base64 encoded data we
    will output the encoded data as decimal integers separated by newlines.
    
    Instead of calling the generic `encode` function that was used by the
    encoder and the decoder before, we implement the encode function using
    Boost's implementation of modular exponentiation. After ensuring correct
    encoding and decoding we can use `modulo_multiply` instead.

--- src/rsa_encode.cpp
+++ src/rsa_encode.cpp
@@ -3,82 +3,57 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <tuple>
 #include <vector>
 #include <boost/multiprecision/cpp_int.hpp>
-#include "base64.h"
-#include "rsa.h"
-#include "rsa_code.h"
 
 using namespace boost::multiprecision;
 typedef number<cpp_int_backend<>, et_off> int_type;
 
+template <typename I, typename O>
+O encode(I f_i, I l_i, const int_type& key, const int_type& modulus, O f_o)
+{
+    while (f_i != l_i) {
+        int_type value = *f_i++;
+        value = powm(value, key, modulus);
+        *f_o++ = value;
+    }
+    return f_o;
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 4) {
-        std::cerr << "Usage: rsa-encode public_key input output\n";
+        std::cerr << "Usage: rsa-encode <public_key> <input> <output>\n";
         return EXIT_FAILURE;
     }
 
     const char* public_key_filename = argv[1];
     std::ifstream public_key_file(public_key_filename);
     if (!public_key_file) {
-        std::cerr << "rsa-encode: error: ";
-        std::cerr << public_key_filename << ": ";
-        std::cerr << strerror(errno) << '\n';
-        return EXIT_FAILURE;
-    }
-
-    std::vector<char> public_key_base64(
-        (std::istream_iterator<char>(public_key_file)),
-         std::istream_iterator<char>());
-    size_t expected_size = rks::base64_size_encoded(
-        RSA_PUBLIC_KEY_SIZE + RSA_MODULUS_KEY_SIZE);
-    if (public_key_base64.size() != expected_size) {
-        std::cerr << public_key_filename << ": error: ";
-        std::cerr << "bad public key size\n";
-        return EXIT_FAILURE;
-    }
-
-    std::vector<unsigned char> data;
-    auto t = rks::base64_decode(public_key_base64.begin(),
-                                public_key_base64.size(),
-                                std::back_inserter(data));
-    if (std::get<1>(t) != 0) {
-        std::cerr << public_key_filename << ": error: ";
-        std::cerr << "bad base64 format\n";
+        std::cerr << "rsa-encode: error: " << public_key_filename << ": " << strerror(errno) << '\n';
         return EXIT_FAILURE;
     }
 
     int_type pub, n;
-    import_bits(pub, data.begin(), data.begin() + RSA_PUBLIC_KEY_SIZE);
-    import_bits(n, data.begin() + RSA_PUBLIC_KEY_SIZE, data.end());
+    public_key_file >> pub >> n;
 
     const char* input_filename = argv[2];
     std::ifstream input_file(input_filename);
     if (!input_file) {
-        std::cerr << "rsa-encode: error: ";
-        std::cerr << input_filename << ": ";
-        std::cerr << strerror(errno) << '\n';
+        std::cerr << "rsa-encode: error: " << input_filename << ": " << strerror(errno) << '\n';
         return EXIT_FAILURE;
     }
 
-    std::vector<unsigned char> input(
-        (std::istreambuf_iterator<char>(input_file)),
-        std::istreambuf_iterator<char>());
-    data.clear();
-    encode(input.begin(), input.size(), RSA_ENCODE_BLOCK_SIZE,
-           std::back_inserter(data), pub, n);
+    std::vector<char> input((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    std::vector<int_type> output;
+    encode(input.begin(), input.end(), pub, n, std::back_inserter(output));
 
     const char* output_filename = argv[3];
     std::ofstream output_file(output_filename);
     if (!output_file) {
-        std::cerr << "rsa-encode: error: ";
-        std::cerr << output_filename << ": ";
-        std::cerr << strerror(errno) << '\n';
+        std::cerr << "rsa-encode: error: " << output_filename << ": " << strerror(errno) << '\n';
         return EXIT_FAILURE;
     }
 
-    rks::base64_encode(data.begin(), data.end(),
-                       std::ostream_iterator<char>(output_file));
+    std::copy(output.begin(), output.end(), std::ostream_iterator<int_type>(output_file, "\n"));
 }
