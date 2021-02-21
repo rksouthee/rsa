b47aa81c56c5359fdcf15625675ff2892d38a51e
Robert Southee <rksouthee@gmail.com>
2021-02-21 12:48:16 +0000

    Add test for odd function
    Added test function for odd and using a single file for testing rsa
    maths functions rather than using individual files.

--- tests/rsa.cpp
+++ tests/rsa.cpp
@@ -8,3 +8,11 @@ TEST_CASE("Check number is even", "[even]")
 	REQUIRE_FALSE(even(1));
 	REQUIRE_FALSE(even(29));
 }
+
+TEST_CASE("Check number is odd", "[odd]")
+{
+	REQUIRE(odd(1));
+	REQUIRE(odd(29));
+	REQUIRE_FALSE(odd(0));
+	REQUIRE_FALSE(odd(74));
+}
